a2479cfc4d06aed5e6dd87126f7698f61400ef61
Jeimy Arias <tr2303100@gmail.com>
2022-07-02 12:49:20 -0500

    All the requested test in the same file

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -17,9 +17,48 @@ describe("First Api Tests", () => {
       city: "New York",
     };
 
-    const response = await axios.get("https://httpbin.org/get", { params: query });
+    const response = await axios.get("https://httpbin.org/get", {
+      params: query,
+    });
 
     expect(response.status).to.eql(StatusCodes.OK);
     expect(response.data.args).to.eql(query);
+  });
+
+  it("Consume HEAD service", async () => {
+    const response = await axios.head("https://httpbin.org");
+
+    expect(response.status).to.equal(StatusCodes.OK);
+  });
+
+  it("Consume PATCH Service", async () => {
+    const data = {
+      name: "Luis",
+      age: "22",
+      city: "Florida",
+    };
+
+    const response = await axios.patch("https://httpbin.org/patch", data);
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(JSON.parse(response.data.data)).to.eql(data);
+  });
+
+  it("Consume PUT Service", async () => {
+    const data = {
+      name: "Tatiana",
+      age: "22",
+      city: "Bogota",
+    };
+
+    const response = await axios.put("https://httpbin.org/put", data);
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(JSON.parse(response.data.data)).to.eql(data);
+  });
+
+  it("Consume DELETE service", async () => {
+    const response = await axios.delete("https://httpbin.org/delete");
+    expect(response.status).to.eql(StatusCodes.OK);
   });
 });
