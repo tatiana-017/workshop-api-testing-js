5889abd9d338c2c9716c2818fc0f517ac33ac5c7
Jeimy Arias <85374580+tatiana-017@users.noreply.github.com>
2022-07-20 21:55:01 -0500

    Static Code Verification (#6)
    Co-authored-by: Jeimy Arias <tr2303100@gmail.com>

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,64 +1,64 @@
-const axios = require("axios");
-const { expect } = require("chai");
-const { StatusCodes } = require("http-status-codes");
+const axios = require('axios');
+const { expect } = require('chai');
+const { StatusCodes } = require('http-status-codes');
 
-describe("First Api Tests", () => {
-  it("Consume GET Service", async () => {
-    const response = await axios.get("https://httpbin.org/ip");
+describe('First Api Tests', () => {
+  it('Consume GET Service', async () => {
+    const response = await axios.get('https://httpbin.org/ip');
 
     expect(response.status).to.equal(StatusCodes.OK);
-    expect(response.data).to.have.property("origin");
+    expect(response.data).to.have.property('origin');
   });
 
-  it("Consume GET Service with query parameters", async () => {
+  it('Consume GET Service with query parameters', async () => {
     const query = {
-      name: "John",
-      age: "31",
-      city: "New York",
+      name: 'John',
+      age: '31',
+      city: 'New York'
     };
 
-    const response = await axios.get("https://httpbin.org/get", {
-      params: query,
+    const response = await axios.get('https://httpbin.org/get', {
+      params: query
     });
 
     expect(response.status).to.eql(StatusCodes.OK);
     expect(response.data.args).to.eql(query);
   });
 
-  it("Consume HEAD service", async () => {
-    const response = await axios.head("https://httpbin.org");
+  it('Consume HEAD service', async () => {
+    const response = await axios.head('https://httpbin.org');
 
     expect(response.status).to.equal(StatusCodes.OK);
   });
 
-  it("Consume PATCH Service", async () => {
+  it('Consume PATCH Service', async () => {
     const data = {
-      name: "Luis",
-      age: "22",
-      city: "Florida",
+      name: 'Luis',
+      age: '22',
+      city: 'Florida'
     };
 
-    const response = await axios.patch("https://httpbin.org/patch", data);
+    const response = await axios.patch('https://httpbin.org/patch', data);
 
     expect(response.status).to.equal(StatusCodes.OK);
     expect(JSON.parse(response.data.data)).to.eql(data);
   });
 
-  it("Consume PUT Service", async () => {
+  it('Consume PUT Service', async () => {
     const data = {
-      name: "Tatiana",
-      age: "22",
-      city: "Bogota",
+      name: 'Tatiana',
+      age: '22',
+      city: 'Bogota'
     };
 
-    const response = await axios.put("https://httpbin.org/put", data);
+    const response = await axios.put('https://httpbin.org/put', data);
 
     expect(response.status).to.equal(StatusCodes.OK);
     expect(JSON.parse(response.data.data)).to.eql(data);
   });
 
-  it("Consume DELETE service", async () => {
-    const response = await axios.delete("https://httpbin.org/delete");
+  it('Consume DELETE service', async () => {
+    const response = await axios.delete('https://httpbin.org/delete');
     expect(response.status).to.eql(StatusCodes.OK);
   });
 });
